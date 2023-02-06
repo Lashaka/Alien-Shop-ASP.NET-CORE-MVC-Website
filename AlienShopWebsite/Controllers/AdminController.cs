cf0446d7b98c0f016a08c67f03f893c3e6e770fe
Victor Rutskin <Lashaka250375@gmail.com>
2023-02-06 19:19:37 +0200

    update admin

--- AlienShopWebsite/Controllers/AdminController.cs
+++ AlienShopWebsite/Controllers/AdminController.cs
@@ -1,12 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlienShopWebsite.Models;
+using AlienShopWebsite.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlienShopWebsite.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private Irepository irepository;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _HostDataBase;
+
+        public AdminController(Microsoft.AspNetCore.Hosting.IHostingEnvironment _HostingContext, Irepository _irepository)
         {
-            return View();
+            this.irepository = _irepository;
+            this._HostDataBase = _HostingContext;
+
+        }
+        public IActionResult Index(int Id)
+        {
+            ViewBag.Categories = irepository.GetCategories();
+            if (Id == 0)
+                return View(irepository.GetAliens());
+            else
+            {
+                return View(irepository.ByCategory(Id));
+            }
+        }
+
+        public IActionResult DeleteAlien(int Id)
+        {
+            irepository.Delete(Id);
+            return RedirectToAction("Index", irepository.GetAliens());
+        }
+
+        [HttpGet]
+        public IActionResult EditAlien(int id)
+        {
+            ViewBag.Categories = irepository.GetCategories();
+            return View(irepository.GetAlienById(id));
+        }
+        [HttpPost]
+        public IActionResult EditAlien(int id, Alien alien)
+        {
+            string UniqeFileName = null;
+            if (
+                ModelState.ErrorCount == 1 &&
+                ModelState.GetFieldValidationState("PicturePath") == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                string UploadFolder = Path.Combine(_HostDataBase.WebRootPath, "Images");
+                UniqeFileName = Guid.NewGuid().ToString() + "_" + alien.File!.FileName;
+                string FilePath = Path.Combine(UploadFolder, UniqeFileName);
+                alien.File.CopyTo(new FileStream(FilePath, FileMode.Create));
+            }
+            Alien NewAlien = new Alien
+            {
+                Name = alien.Name,
+                Age = alien.Age,
+                CategoryId = alien.CategoryId,
+                Descripition = alien.Descripition,
+                PicturePath = "Images\\" + UniqeFileName
+            };
+
+            irepository.UpdateAlien(id, NewAlien);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult AddAlien()
+        {
+            ViewBag.Categories = irepository.GetCategories();
+            return View(new Alien());
+        }
+        [HttpPost]
+        public IActionResult AddAlien(Alien alien)
+        {
+            string UniqeFileName = null;
+
+            if (
+                ModelState.ErrorCount == 1 &&
+                ModelState.GetFieldValidationState("PicturePath") == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                string UploadFolder = Path.Combine(_HostDataBase.WebRootPath, "Images");
+                UniqeFileName = Guid.NewGuid().ToString() + "_" + alien.File!.FileName;
+                string FilePath = Path.Combine(UploadFolder, UniqeFileName);
+                alien.File.CopyTo(new FileStream(FilePath, FileMode.Create));
+            }
+            Alien NewAlien = new Alien
+            {
+                Name = alien.Name,
+                Age = alien.Age,
+                CategoryId = alien.CategoryId,
+                Descripition = alien.Descripition,
+                PicturePath = "Images\\" + UniqeFileName
+            };
+            irepository.AddAlien(NewAlien);
+            return RedirectToAction("Index", "Catalog");
         }
     }
 }
--- AlienShopWebsite/Controllers/CatalogController.cs
+++ AlienShopWebsite/Controllers/CatalogController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlienShopWebsite.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AlienShopWebsite.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Index()
+        private Irepository irepository;
+
+        public CatalogController(Irepository _iripository)
         {
-            return View();
+            this.irepository = _iripository;
+        }
+        public IActionResult Index(int Id)
+        {
+            ViewBag.Categories = irepository.GetCategories();
+            if (Id == 0)
+                return View(irepository.GetAliens());
+            else
+            {
+                return View(irepository.ByCategory(Id));
+            }
+        }
+        public IActionResult Details(int Id)
+        {
+
+            var comments = irepository.ShowComments(Id);
+            var animals = irepository.GetAlienById(Id);
+            return View(animals);
+        }
+        [HttpPost]
+        public IActionResult AddComment(int AnimalId, string comment)
+        {
+            if (comment.IsNullOrEmpty() || comment.Length > 100)
+            {
+
+                return RedirectToAction("Details", "Catalog", new { id = AnimalId });
+            }
+            else
+            {
+                var animals = irepository.GetAlienById(AnimalId);
+                irepository.AddComments(comment, AnimalId);
+            }
+            return RedirectToAction("Details", "Catalog", new { id = AnimalId });
         }
     }
 }
